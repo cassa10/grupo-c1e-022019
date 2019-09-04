0f4fd4916ec5245c9f1fe9092e449a04dacc7c0e
NicoAlvarez <nicolas.alv3@gmail.com>
2019-09-03 21:09:58 -0300

    setup inicial

---
+++ src/main/java/com/desapp/grupoc1e022019/GrupoC1e022019Application.java
@@ -0,0 +1,13 @@
+package com.desapp.grupoc1e022019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoC1e022019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoC1e022019Application.class, args);
+	}
+
+}
