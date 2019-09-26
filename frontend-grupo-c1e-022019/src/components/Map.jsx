f754425a971aa6bb700d0e8e56dad59f8686a3c1
NicoAlvarez <nicolas.alv3@gmail.com>
2019-09-26 16:59:53 -0300

    Adding measures to map

--- frontend-grupo-c1e-022019/src/components/Map.jsx
+++ frontend-grupo-c1e-022019/src/components/Map.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../dist/css/Map.css';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Marker, Popup, TileLayer,withLeaflet } from 'react-leaflet';
+import MeasureControlDefault from 'react-leaflet-measure';
+const MeasureControl = withLeaflet(MeasureControlDefault);
 
 class Mapa extends React.Component {
   constructor(props) {
@@ -11,16 +13,28 @@ class Mapa extends React.Component {
 
   render() {
     const position = [-34.706667, -58.2775];
+    const measureOptions = {
+      position: 'topright',
+      primaryLengthUnit: 'meters',
+      secondaryLengthUnit: 'kilometers',
+      primaryAreaUnit: 'sqmeters',
+      secondaryAreaUnit: 'acres',
+      activeColor: '#db4a29',
+      completedColor: '#9b2d14'
+    };
     return (
-      <Map center={position} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-        </Marker>
-      </Map>
+      <div>
+        <Map center={position} zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          <Marker position={position}>
+            <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+          </Marker>
+          <MeasureControl {...measureOptions} />
+        </Map>
+      </div>
     );
   }
 }
